60043ab09f67d01143d4bf840ebb45046300802b
colezzz <avismusthafa1601@gmail.com>
2026-05-18 17:34:38 +0700

    feat: tambah command UNO dan info warna aktif

--- src/pendukung.pl
+++ src/pendukung.pl
@@ -1,5 +1,3 @@
-% pendukung.pl
-
 % LIHAT COMMAND
 
 lihatCommand :-
@@ -7,12 +5,14 @@ lihatCommand :-
     write('Aksi utama yang tersedia:'), nl,
     write('1. mainkanKartu(N)'), nl,
     write('2. ambilKartu'), nl,
+    write('3. uni(N)'), nl,
+    write('4. tantang'), nl,
     nl,
     write('Aksi pendukung yang tersedia:'), nl,
     write('1. lihatCommand'), nl,
     write('2. lihatKartu'), nl,
-    write('3. cekInfo'), nl.
-
+    write('3. cekInfo'), nl,
+    write('4. tangkap(NamaPemain)'), nl.
 
 % LIHAT KARTU
 
@@ -23,13 +23,11 @@ lihatKartu :-
     write('Berikut kartu yang anda miliki.'), nl,
     tampilkan_list_kartu(ListKartu, 1).
 
-% rekursif tampilkan kartu dengan nomor urut
 tampilkan_list_kartu([], _).
 tampilkan_list_kartu([kartu(W, J)|Rest], N) :-
     format('~w. ~w-~w~n', [N, W, J]),
     N1 is N + 1,
     tampilkan_list_kartu(Rest, N1).
-
 
 % CEK INFO
 
@@ -37,14 +35,14 @@ cekInfo :-
     nl,
     discard_top(kartu(W, J)),
     format('Kartu discard top: ~w-~w.~n', [W, J]),
+    warna_aktif(Warna),
+    format('Warna aktif: ~w.~n', [Warna]),
     urutan_pemain(Urutan),
     write('Urutan pemain: '),
     tampilkan_urutan(Urutan),
     nl,
     tampilkan_info_semua_pemain(Urutan, 1).
 
-% tampilkan nama dan jumlah kartu tiap pemain
-% pakai hitung_panjang dari state.pl, bukan hitung_jumlah_kartu sendiri
 tampilkan_info_semua_pemain([], _).
 tampilkan_info_semua_pemain([Pemain|Rest], N) :-
     kartu_pemain(Pemain, ListKartu),
